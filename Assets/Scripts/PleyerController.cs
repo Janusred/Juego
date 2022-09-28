0e93154663665834551160239293cf61a2f795f1
Janusred <horaciok300@hotmail.com>
2022-09-28 02:11:47 -0500

    Creacion de Scripts

---
+++ Assets/Scripts/PleyerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleyerController : MonoBehaviour
+{
+
+    //Variable de movimiento del monito ese!!
+    public float jumpForce = 6f;
+    Rigibody2D PlayerRigibody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Jump{
+
+    }
+}
