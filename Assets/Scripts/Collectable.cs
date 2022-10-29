38684fa93bec41170cca00dc861394d9362f84c1
Janusred <horaciok300@hotmail.com>
2022-10-29 03:29:21 -0500

    Creacion de monedas y recollecion de monedas con Scrips

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public emun CollectableType{
+    healthPotion
+    manaPotion
+    money
+}
+
+public class Collectable : MonoBehaviour
+
+public CollectableType type = CollectableType.money;
+
+private SprinteRenderer sprite;
+private CircleCollider2D itemCollider;
+
+bool hasBeenCollected = false;
+
+public int value = 1;
+
+private void Awake(){
+    sprite = GetComponent<SprinteRenderer>();
+    itemCollider = GetComponent<CircleCollider2D>();
+}
+
+{
+    void Show(){
+        sprite.enabled= true;
+        itemCollider.enabled= true;
+        hasBeenCollected=false;
+    }    
+    void Hide(){
+        sprite.enabled=false;
+        itemCollider.enabled= false;
+    }
+
+    void Collect(){
+        Hide();
+        hasBeenCollected= true;
+
+        switch (this.type){
+            case CollectableType.money:
+
+            break;
+            case CollectableType.healthPotion:
+            break;
+            case CollectableType.manaPotion:
+            break;
+        }
+        {
+            
+            default:
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collison){
+        if(collision.tag == tag "Player"){
+            Destroy(gameObject);
+        }
+    }
+}
